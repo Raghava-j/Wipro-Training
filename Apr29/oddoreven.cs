6fda0b31d88686262fa2943ee76dcc4339b26ed7
Raghava <jalluriraghava@gmail.com>
2025-04-29 16:07:59 +0530

    code-3

---
+++ Apr29/oddoreven.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int num = 11;
+        if (num %2 == 0)
+        {
+            Console.WriteLine("The num is even");
+        }
+        else
+        {
+            Console.WriteLine("The num is odd");
+        }
+        
+    }
+}
