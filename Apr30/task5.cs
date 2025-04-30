bb5f2e72550209321dd998f407bc7bddaf2b8476
Raghava <jalluriraghava@gmail.com>
2025-04-30 15:34:17 +0530

    code

---
+++ Apr30/task5.cs
@@ -0,0 +1,18 @@
+class Proogram
+{
+    static int Add(int a, int b)
+    {
+        return a + b;
+    }
+    static double Add(double a, double b)
+    {
+        return a + b;
+    }
+    static void Main()
+    {
+        Console.WriteLine("Int Add: " + Add(3, 4));
+        //7
+        Console.WriteLine("Doble Add: " + Add(2.5, 4.3));
+        // 6.8
+    }
+}
