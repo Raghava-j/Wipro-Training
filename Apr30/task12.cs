d186975c21ed2344ef7362c4795cef10204bd701
Raghava <jalluriraghava@gmail.com>
2025-04-30 15:46:57 +0530

    code

---
+++ Apr30/task12.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int i = 6;
+        
+        do 
+        {
+            Console.WriteLine(i);
+            i++;
+        } while (i <= 5);
+    }
+}
