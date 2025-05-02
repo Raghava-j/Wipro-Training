b07cc6e13f7ad69028bc31c6bf25ddd2274de520
Raghava <jalluriraghava@gmail.com>
2025-05-02 15:29:30 +0530

    code

--- May2/ass-6-PrintArrayElements.cs
+++ May2/ass-6-PrintArrayElements.cs
@@ -3,13 +3,13 @@ using System;
 class Program
 {
     // Recursive method to print array elements
-    static void PrintArray(int[] arr, int index)
+    static void PrintArrayRecursively(int[] arr, int index)
     {
         if (index == arr.Length)
             return;
 
         Console.WriteLine(arr[index]);
-        PrintArray(arr, index + 1);
+        PrintArrayRecursively(arr, index + 1);
     }
 
     static void Main()
@@ -17,6 +17,6 @@ class Program
         int[] numbers = { 1, 2, 3, 4, 5, 6, 7 };
 
         Console.WriteLine("Array elements:");
-        PrintArray(numbers, 0);  // Start from index 0
+        PrintArrayRecursively(numbers, 0);  // Start from index 0
     }
 }
