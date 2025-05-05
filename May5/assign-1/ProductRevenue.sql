e5e9c11d7d870d76904457a4ff287e9b9df2718e
Raghava Ranjith <jalluriraghava@gmail.com>
2025-05-05 10:34:41 +0530

    sql

---
+++ May5/assign-1/ProductRevenue.sql
@@ -0,0 +1,12 @@
+CREATE PROCEDURE GetProductRevenue
+     @ProductID INT
+AS
+BEGIN
+   SELECT
+      p.ProductID,
+	  p.ProductName,
+	  SUM(o.Quantity * p.Price) AS TotalRevenue
+   FROM  Orders o
+   INNER JOIN Products p ON o.ProductID = p.ProductID
+   WHERE o.ProductID = @ProductID
+END;
