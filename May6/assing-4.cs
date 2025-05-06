7442bb80f52866eb625dc0b807b17ac648fdbcef
Raghava Ranjith <jalluriraghava@gmail.com>
2025-05-06 15:38:12 +0530

    cs

---
+++ May6/assing-4.cs
@@ -0,0 +1,21 @@
+using system;
+class Program
+{
+    static void Main()
+    {
+        int n = 4;
+        PrintTriangle(n);
+
+    }
+    static void PrintTriangle(int n)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("* ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
