94bdf01ff12b15a20f3e097603b7f0b5014b4bfe
Raghava Ranjith <jalluriraghava@gmail.com>
2025-05-08 16:21:25 +0530

    cs

--- May8/assign--3.cs
+++ May8/assign--3.cs
@@ -6,8 +6,27 @@ class Program
     static void Main()
     {
         ArrayList numbers = new ArrayList();
-        Random random = new Random();
+        Random rand = new Random();
 
         for (int i = 0; i < 10; i++)
+           numbers.Add(rand.Next(1, 51));
+
+           numbers.Sort();
+
+           Console.WriteLine("Sorted numbers: ");
+           foreach (int num in numbers)
+               Console.WriteLine(num+ " ");
+           Console.WriteLine();
+
+           int min = (int)numbers[0];
+           int max = (int)numbers[numbers.Count - 1];
+           Console.WriteLine($"Minimum: {min}");
+           Console.WriteLine($"Maximum: {max}");
+
+           int sum = 0;
+           foreach (int num in numbers)
+               sum += num;
+           double average = (double)sum / numbers.Count;
+           Console.WriteLine($"Average: {average:F2}");
     }
 }
