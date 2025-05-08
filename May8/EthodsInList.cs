caf375038f0724a06d16e136897cbdc41ef4faa7
Raghava Ranjith <jalluriraghava@gmail.com>
2025-05-08 09:22:45 +0530

    cs

---
+++ May8/EthodsInList.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> fruits = new List<string> () ("Apple", "Banana", "Cherry");
+
+        // Adding an element
+        fruits.Add("Mango");
+
+        //Inserting an element at index 1
+        fruits.Insert(1, "Orange");
+
+        // Removing an element by value
+        fruits.Remove("Banana");
+
+        // Checking if an element exists
+        if (fruits.Contains("Apple"))
+             Console.WriteLine("Apple is in the list.");
+
+        // Sorting the list
+        fruits.Sort();
+
+        // Printing the final list
+        Console.WriteLine("fruits List:");
+        foreach (string fruit in fruits)
+        {
+            Console.WriteLine(fruit);
+        }
+    }
+}
