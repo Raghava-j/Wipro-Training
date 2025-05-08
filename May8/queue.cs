81b73e59695b638221f8fa9029744a434d783f7b
Raghava Ranjith <jalluriraghava@gmail.com>
2025-05-08 14:39:41 +0530

    cs

---
+++ May8/queue.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Queue<string> customers = new Queue<string>();
+
+        // Adding customers to the queue
+        customers.Enqueue("Rajesh");
+        customers.Enqueue("Priya");
+        customers.Enqueue("Neha");
+
+        Console.WriteLine("Serving Customers:");
+        while (customers.Count > 0)
+        {
+            Console.WriteLine("Serving: " + customers.Dequeue());
+        }
+    }
+}
