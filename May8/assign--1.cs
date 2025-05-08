197a18adcdb177b6626f6af8e66c247227da5b39
Raghava Ranjith <jalluriraghava@gmail.com>
2025-05-08 10:18:10 +0530

    cs

---
+++ May8/assign--1.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> employees = new List<string>();
+
+        employees.Add("Bhaskar");
+        employees.Add("Bhanu");
+        employees.Add("David");
+        employees.Add("Srinu");
+        employees.Add("Katari");
+
+        employees.Remove("Bhanu");
+
+        string check = "David";
+        if (employees.Contains(check))
+        {
+            Console.WriteLine($"{check} is in the employee list.");
+        }
+        else
+        {
+            Console.WriteLine($"{check} is not in the employee list.");
+        }
+        Console.WriteLine("Total number of employees: " + employees.Count);
+    }
+}
