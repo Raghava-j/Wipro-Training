691bc73a8f162699ed98193c290d616e06f59b41
Raghava Ranjith <jalluriraghava@gmail.com>
2025-05-09 16:14:15 +0530

    cs

---
+++ May9/assign--3.cs
@@ -0,0 +1,24 @@
+using System;
+class Shape
+{
+    public virtual void Draw()
+    {
+        Console.WriteLine("Drawing a Shape");
+    }
+}
+
+class Circle : Shape
+{
+    public override void Draw()
+    {
+        Console.WriteLine("Drawing a Circle");
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Shape myShape = new Circle();
+        myShape.Draw();
+    }
+}
