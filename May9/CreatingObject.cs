1e0d2ffb582a8385a0c092cc503be335beccd6e9
Raghava Ranjith <jalluriraghava@gmail.com>
2025-05-09 09:23:10 +0530

    cs

---
+++ May9/CreatingObject.cs
@@ -0,0 +1,34 @@
+using System;
+
+class Student
+{
+    // Fields
+    public int RollNumber;
+    public string Name;
+    public int Marks;
+
+    // Method to print Student Details
+    public void PrintDetails()
+    {
+        Console.WriteLine($"Roll Number: {RollNumber}");
+        Console.WriteLine($"Name: {Name}");
+        Console.WriteLine($"Marks: {Marks}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Creating an object of students
+        Student student1 = new Student();
+
+        //Assiging values
+        student1.RollNumber = 101;
+        student1.Name = "Amit";
+        student1.Marks = 85;
+
+        // Printing details
+        student1.PrintDetails();
+    }
+}
