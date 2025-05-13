67c10966e96643f9155ef8c1ba23fd2ef075f1ed
Raghava Ranjith <jalluriraghava@gmail.com>
2025-05-13 12:24:35 +0530

    cs

---
+++ May13/SimpleDelegate.cs
@@ -0,0 +1,18 @@
+class SimpleDelegate
+{
+    public delegate int SomeOperation(int i, int j);
+
+    class Program
+    {
+        static int Sum(int x, int y)
+        {
+            return x + y;
+        }
+        static void Main(string[] args)
+        {
+            SomeOperation add = Sum;
+            int result = add(10, 10);
+             Console.WriteLine(result);
+         }
+    }
+}
