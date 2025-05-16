6e1c4f9ac8d20d8fd7c454d536a23b101a9bf8ce
Raghava Ranjith <jalluriraghava@gmail.com>
2025-05-16 15:46:57 +0530

    cs

--- May16/assign-1.cs
+++ May16/assign-1.cs
@@ -47,3 +47,79 @@ class SqlConnectionDemo
         }
     }
 }
+/*
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Runtime.Intrinsics.Arm;
+
+namespace Lesson04
+{
+    class ReaderDemo
+    {
+        static void Main()
+        {
+            ReaderDemo rd = new ReaderDemo();
+            rd.SimpleRead();
+        }
+
+        public void SimpleRead()
+        {
+            // delclare the sqldatareader, which is used in
+            //both the try block and finally block
+
+
+            SqlDataReader rdr = null;
+
+            // create a connection object
+            SqlConnection conn = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Northwind;"
+                + "Integrated Security=true");
+
+            SqlCommand cmd = new SqlCommand("select * from customers", conn);
+
+            try
+            {
+                // open the connection
+                conn.Open();
+
+                //1. get an instance of the sqlDataReader
+                rdr = cmd.ExecuteReader();
+
+                // print a set of column headers
+                Console.WriteLine("Contact Name               City                 Company Name");
+                Console.WriteLine("-------------              ------------         -----------");
+
+                //2. print necessary columns of each record
+                while (rdr.Read())
+                {
+                    // get the results of each column
+                    string contact = (string)rdr["ContactName"];
+                    string company = (string)rdr["CompanyName"];
+                    string city = (string)rdr["City"];
+
+                    // PRintout the results
+                    Console.Write("{0,-25}", contact);
+                    Console.Write("{0,-20}", city);
+                    Console.Write("{0,-40}", company);
+                    Console.WriteLine();
+                }
+
+            }
+            finally
+            {
+                // close the reader
+                if (rdr != null)
+                {
+                    rdr.Close();
+                }
+
+                //5. Close the connection
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+        }
+    }
+}
+*/
