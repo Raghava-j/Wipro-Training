2a7c86bb36015939d9d76c917e88f8acf30aa86d
Raghava Ranjith <jalluriraghava@gmail.com>
2025-06-02 17:00:45 +0530

    web api

---
+++ June02/WebApplication18/WebApplication18/Data/WebApplication18Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplication18.Models;
+
+namespace WebApplication18.Data
+{
+    public class WebApplication18Context : DbContext
+    {
+        public WebApplication18Context (DbContextOptions<WebApplication18Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplication18.Models.Student> Student { get; set; } = default!;
+    }
+}
